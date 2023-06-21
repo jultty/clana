c9f438e61ea7064a519b37725b3f4950a7a255a7
jultty <juno.t@tuta.io>
2023-06-21 09:57:38 -0300

    Add a struct definition to header

--- include/header.hpp
+++ include/header.hpp
@@ -4,6 +4,17 @@
 #include <iostream>
 
 using namespace std;
+using Header = struct Header;
+
+struct Header {
+  int column = -1;
+  string content = "";
+  int total = -1;
+  float average = -1;
+  Line* line = nullptr;
+  Field* next = nullptr;
+  Field* previous = nullptr;
+};
 
 bool detect_headers(string);
 string get_header(int, Line*);

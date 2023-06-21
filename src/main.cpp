35dd7e7811e3b61696e29dad3013fb60fc1646ef
jultty <juno.t@tuta.io>
2023-06-21 04:01:59 -0300

    Add line gap detection

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,14 @@ int main () {
 
   // set up I/O stream
   ifstream infile;
+  ifstream fake_infile;
   ofstream outfile;
   infile.open("in.csv");
+  fake_infile.open("in2.csv");
 
   // iterate over each input line
   File file;
+  File fake_file;
   string line_content;
 
   while (getline (infile, line_content))
@@ -20,15 +23,37 @@ int main () {
 
   infile.close();
 
-  // write output to a file
-  outfile.open ("out.log");
-  outfile << "File I/O OK\n";
-  outfile.close();
+  while (getline (fake_infile, line_content))
+    fake_file.add(line_content);
 
-  // print mapped contents
+  infile.close();
+  fake_infile.close();
+
+  //print mapped contents
   file.print("");
 
-  cout << "Chuva 22 is at column" << get_column("Chuva22", file.headers) << endl;
+  // demos
+
+  // find the first field with a gap
+  Field* empty = field_gap_scan(file.last);
+  cout << "Found empty field: " << empty->header;
+  cout << " at row " << file.last->row;
+  cout << " column " << empty->column << endl;
+
+  // find the first line with a gap
+  Line* fake_line = traverse_lines(fake_file.first, 45);
+  Line* gap_line = line_gap_scan(fake_line);
+  Field* gap_field = field_gap_scan(gap_line);
+
+  if (gap_field  -1) {
+    cout << "Line gap scan started at " << fake_line->row;
+    cout << ", first gap found on line " << gap_line->row;
+    cout << ", column " << gap_field->column << " (";
+    cout << gap_field->header << ")" << endl;
+  }
+
+  cout << "Chuva 22 is at column";
+  cout << get_column("Chuva22", file.headers) << endl;
 
   return 0;
 }

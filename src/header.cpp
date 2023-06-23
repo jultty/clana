d9192750811e87d67f7e7c4cc6e60ddeaf2e226c
jultty <juno.t@tuta.io>
2023-06-23 10:21:28 -0300

    Implement separate header functions and a float parser

--- src/header.cpp
+++ src/header.cpp
@@ -10,18 +10,12 @@ bool detect_headers(string line)
   return line.substr(0, HEADER_PATTERN.size()) == HEADER_PATTERN;
 }
 
-void wait() {
-  cout << " [utils.wait] Hit return to continue";
-  cout.flush();
-  while (getchar() != '\n');
-}
+// traverses a linked list of headers
+Header* traverse_headers(Line* headers, int start, int distance) {
 
-// traverses a linked list of fields
-Field* traverse_fields(Line* line, int start, int distance) {
+  Header* current_position = headers->first_field->header;
 
-  Field* current_position = line->first_field;
-
-  distance += start - 1; // traversing will start current_position position 1
+  distance += start - 1; // traversing will start at position 1
 
   while (distance != 0) {
     if (distance > 0) {
@@ -36,12 +30,12 @@ Field* traverse_fields(Line* line, int start, int distance) {
 }
 
 // binary search for a column's header
-string search_header(Line* headers, int target, int first, int last) {
+Header* search_header(Line* headers, int target, int first, int last) {
 
   int pivot = (first + last) / 2;
   int distance = target - pivot;
   Field* traversed_field = traverse_fields(headers, pivot, distance);
-  string target_header = traversed_field->content;
+  Header* target_header = traversed_field->header;
 
   if (target > pivot) {
     search_header(headers, target, pivot + 1, last);
@@ -52,12 +46,12 @@ string search_header(Line* headers, int target, int first, int last) {
 }
 
 // search for a header by its column
-string get_header(int column, Line* headers) {
+Header* get_header(int column, Line* headers) {
 
   int first = headers->first_field->column;
   int last = headers->last_field->column;
 
-  string result = search_header(headers, column, first, last);
+  Header* result = search_header(headers, column, first, last);
   return result;
 };
 
@@ -84,3 +78,22 @@ int get_column(string header, Line* headers) {
   return start->column;
 };
 
+double parse_double(string observation) {
+  string parsed_observation = "";
+  for (char character : observation) {
+        if (character == ',') {
+          parsed_observation.push_back('.');
+        }
+          else if (isdigit(character)) {
+          parsed_observation.push_back(character);
+        } else {
+          parsed_observation = "0";
+        }
+    }
+
+  if (parsed_observation == "") {
+    parsed_observation = "0";
+  }
+  double parsed_double = stod(parsed_observation);
+  return parsed_double;
+};

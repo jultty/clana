4525a55a2135060fee170037a7814cc9ea06f1b5
jultty <juno.t@tuta.io>
2023-06-23 10:23:52 -0300

    Move field traversal to the field functions

--- src/field.cpp
+++ src/field.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 #include "file.hpp"
 #include "field.hpp"
+#include <algorithm>
+#include <string>
+
+// traverses a linked list of fields
+Field* traverse_fields(Line* line, int start, int distance) {
+
+  Field* current_position = line->first_field;
+
+  distance += start - 1; // traversing will start at position 1
+
+  while (distance != 0) {
+    if (distance > 0) {
+      distance--;
+      current_position = current_position->next;
+    } else {
+      current_position = current_position->previous;
+      distance++;
+    }
+  }
+  return current_position;
+}
 
 // finds gaps in lines
 // returns the first field if none found
